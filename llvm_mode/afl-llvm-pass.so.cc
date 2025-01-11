bda3b945cbc303b90a34cb4b14e79c48c4f5d227
k3ppf0r <2094289254@qq.com>
2025-01-11 20:59:19 +0800

    llvm mode 解读1

--- llvm_mode/afl-llvm-pass.so.cc
+++ llvm_mode/afl-llvm-pass.so.cc
@@ -72,8 +72,9 @@ char AFLCoverage::ID = 0;
 
 bool AFLCoverage::runOnModule(Module &M) {
 
+  // 通过getContext来获取LLVMContext，其保存了整个程序里分配的类型和常量信息。 
   LLVMContext &C = M.getContext();
-
+  // 通过这个Context来获取type实例Int8Ty和Int32Ty
   IntegerType *Int8Ty  = IntegerType::getInt8Ty(C);
   IntegerType *Int32Ty = IntegerType::getInt32Ty(C);
 
@@ -88,7 +89,7 @@ bool AFLCoverage::runOnModule(Module &M) {
   } else be_quiet = 1;
 
   /* Decide instrumentation ratio */
-
+  // 读取环境变量AFL_INST_RATIO给变量inst_ratio, ，其值默认为100，这个值代表一个插桩概率, 本来应该每个分支都必定插桩，而这是一个随机的概率决定是否要在这个分支插桩。
   char* inst_ratio_str = getenv("AFL_INST_RATIO");
   unsigned int inst_ratio = 100;
 
@@ -102,7 +103,7 @@ bool AFLCoverage::runOnModule(Module &M) {
 
   /* Get globals for the SHM region and the previous location. Note that
      __afl_prev_loc is thread-local. */
-
+  // 获取全局变量中指向共享内存的指针，以及上一个基础块的编号
   GlobalVariable *AFLMapPtr =
       new GlobalVariable(M, PointerType::get(Int8Ty, 0), false,
                          GlobalValue::ExternalLinkage, 0, "__afl_area_ptr");
@@ -115,35 +116,36 @@ bool AFLCoverage::runOnModule(Module &M) {
 
   int inst_blocks = 0;
 
-  for (auto &F : M)
-    for (auto &BB : F) {
-
+  for (auto &F : M)  // 遍历每个函数
+    for (auto &BB : F) { // 遍历每个函数中的每个基本块
+      // 找到此基本块中适合插入instrument的位置，后续通过初始化IRBuilder的一个实例进行插入，插在基本块首部
       BasicBlock::iterator IP = BB.getFirstInsertionPt();
       IRBuilder<> IRB(&(*IP));
 
       if (AFL_R(100) >= inst_ratio) continue;
 
       /* Make up cur_loc */
-
+      // 随机创建一个当前基本块的编号
       unsigned int cur_loc = AFL_R(MAP_SIZE);
 
       ConstantInt *CurLoc = ConstantInt::get(Int32Ty, cur_loc);
 
       /* Load prev_loc */
-
+      // 通过插入load指令来获取前一个基本块的编号。
       LoadInst *PrevLoc = IRB.CreateLoad(AFLPrevLoc);
       PrevLoc->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
       Value *PrevLocCasted = IRB.CreateZExt(PrevLoc, IRB.getInt32Ty());
 
       /* Load SHM pointer */
-
+      // 通过插入load指令来获取共享内存的地址，并通过CreateGEP函数来获取共享内存里指定index的地址，这个index通过cur_loc和prev_loc取xor计算得到。
       LoadInst *MapPtr = IRB.CreateLoad(AFLMapPtr);
+      // 为MapPtr设置了名为"nosanitize"的元数据标记
       MapPtr->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
       Value *MapPtrIdx =
           IRB.CreateGEP(MapPtr, IRB.CreateXor(PrevLocCasted, CurLoc));
 
       /* Update bitmap */
-
+      // 通过插入load指令来读取对应index地址，并通过插入add指令来将其加一，然后通过创建store指令将新值写入，更新共享内存
       LoadInst *Counter = IRB.CreateLoad(MapPtrIdx);
       Counter->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
       Value *Incr = IRB.CreateAdd(Counter, ConstantInt::get(Int8Ty, 1));
@@ -151,7 +153,7 @@ bool AFLCoverage::runOnModule(Module &M) {
           ->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
 
       /* Set prev_loc to cur_loc >> 1 */
-
+      // 将当前cur_loc的值右移一位，然后通过插入store指令，更新__afl_prev_loc的值
       StoreInst *Store =
           IRB.CreateStore(ConstantInt::get(Int32Ty, cur_loc >> 1), AFLPrevLoc);
       Store->setMetadata(M.getMDKindID("nosanitize"), MDNode::get(C, None));
